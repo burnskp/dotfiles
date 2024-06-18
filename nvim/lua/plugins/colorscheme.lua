4b0f9f8a8cc195bd9345cac8265ec797704354cd
Kevin Burns <kevin@burnskp.dev>
2024-06-18 14:45:25 -0500

    Fork cobalt2 theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,7 +4,7 @@ return {
 		return not vim.g.vscode
 	end,
 
-	dependencies = { { "tjdevries/colorbuddy.nvim", tag = "v1.0.0" }, "lalitmee/cobalt2.nvim" },
+	dependencies = { { "tjdevries/colorbuddy.nvim", tag = "v1.0.0" }, "burnskp/cobalt2.nvim" },
 	opts = {
 		colorscheme = function()
 			require("colorbuddy").colorscheme("cobalt2")
