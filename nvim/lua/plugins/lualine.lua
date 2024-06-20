56a3901f30a8ea4d2617eaeb011e58e223eb2294
Kevin Burns <kevin@burnskp.dev>
2024-06-20 13:15:44 -0500

    Remove some items from the lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,19 +1,28 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	opts = {
-		options = {
-			section_separators = "",
-			component_separators = "",
-		},
-		sections = {
-			lualine_a = {
-				{
-					"mode",
-					fmt = function(str)
-						return str:sub(1, 1)
-					end,
-				},
+	opts = function(_, opts)
+		-- This should remove the noice command output
+		local line_x = {}
+		for i = 2, #opts.sections.lualine_x do
+			table.insert(line_x, opts.sections.lualine_x[i])
+		end
+		return {
+			options = {
+				section_separators = "",
+				component_separators = "",
 			},
-		},
-	},
+			sections = {
+				lualine_a = {
+					{
+						"mode",
+						fmt = function(str)
+							return str:sub(1, 1)
+						end,
+					},
+				},
+				lualine_x = line_x,
+				lualine_z = {},
+			},
+		}
+	end,
 }
