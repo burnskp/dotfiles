1f01436cee43a0e040295925b8f8b362ae078f7a
Kevin Burns <kevin@burnskp.dev>
2024-07-08 09:14:43 -0500

    Fix dial

--- nvim/lua/plugins/dial.lua
+++ nvim/lua/plugins/dial.lua
@@ -1,74 +1,21 @@
 return {
 	"monaqa/dial.nvim",
-	config = function()
+	opts = function(_, opts)
 		local augend = require("dial.augend")
-		require("dial.config").augends:register_group({
-			default = {
-				augend.integer.alias.decimal_int,
-				augend.integer.alias.hex,
-				augend.date.alias["%Y/%m/%d"],
-				augend.constant.new({
-					elements = { "and", "or" },
-					word = true,
-					cyclic = true,
-				}),
-				augend.constant.new({
-					elements = { "&&", "||" },
-					word = false,
-					cyclic = true,
-				}),
-				augend.constant.new({
-					elements = { "on", "off" },
-					word = true,
-					cyclic = true,
-				}),
-				augend.constant.new({
-					elements = { "yes", "no" },
-					word = true,
-					cyclic = true,
-				}),
-				augend.constant.new({
-					elements = { "first", "last" },
-					word = true,
-					cyclic = true,
-				}),
-				augend.constant.alias.bool,
-				augend.semver.alias.semver,
-			},
-			lua = {
-				augend.constant.new({
-					elements = { "else", "elseif" },
-					word = true,
-					cyclic = true,
-				}),
-			},
-			python = {
-				augend.constant.new({
-					elements = { "else", "elif" },
-					word = true,
-					cyclic = true,
-				}),
-			},
-			go = {
-				augend.constant.new({
-					elements = { "else", "else if" },
-					word = true,
-					cyclic = true,
-				}),
-			},
-			visual = {
-				augend.integer.alias.decimal_int,
-				augend.integer.alias.hex,
-				augend.date.alias["%Y/%m/%d"],
-				augend.constant.alias.alpha,
-				augend.constant.alias.Alpha,
-				augend.semver.alias.semver,
-				augend.constant.new({
-					elements = { "-", "_" },
-					word = false,
-					cyclic = true,
-				}),
-			},
-		})
+		opts.groups.go = opts.groups.go or {}
+
+		local and_or = augend.constant.new({ elements = { "and", "or" } })
+		local on_off = augend.constant.new({ elements = { "on", "off" } })
+		local yes_no = augend.constant.new({ elements = { "yes", "no" } })
+		local else_elseif = augend.constant.new({ elements = { "else", "elseif" } })
+		local else_elif = augend.constant.new({ elements = { "else", "elif" } })
+		local else_else_if = augend.constant.new({ elements = { "else", "else if" } })
+
+		table.insert(opts.groups.default, and_or)
+		table.insert(opts.groups.default, on_off)
+		table.insert(opts.groups.default, yes_no)
+		table.insert(opts.groups.lua, else_elseif)
+		table.insert(opts.groups.python, else_elif)
+		table.insert(opts.groups.go, else_else_if)
 	end,
 }
