9d1ccb6f67be47a12b623c16a2543dfe9126b422
Kevin Burns <kevin@burnskp.com>
2024-07-26 21:10:52 -0500

    Add more copilot shortcuts

--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -9,4 +9,50 @@ return {
 			},
 		},
 	},
+	keys = {
+		{
+			"<leader>aa",
+			function()
+				return require("CopilotChat").open({
+					selection = function()
+						return nil
+					end,
+				})
+			end,
+			desc = "Toggle (CopilotChat)",
+			mode = { "n", "v" },
+		},
+		{
+			"<leader>ad",
+			"<cmd>CopilotChatDocs<cr>",
+			desc = "Document Code",
+			mode = { "n", "v" },
+		},
+		{
+			"<leader>af",
+			"<cmd>CopilotChatFix<cr>",
+			desc = "Fix Code",
+			mode = { "n", "v" },
+		},
+		{
+			"<leader>ao",
+			"<cmd>CopilotChatOptimize<cr>",
+			desc = "Optimize Code",
+			mode = { "n", "v" },
+		},
+		{
+			"<leader>at",
+			"<cmd>CopilotChatTests<cr>",
+			desc = "Add Tests to Code",
+			mode = { "n", "v" },
+		},
+		{
+			"<leader>as",
+			function()
+				return require("CopilotChat").toggle()
+			end,
+			desc = "Toggle (CopilotChat)",
+			mode = { "n", "v" },
+		},
+	},
 }
