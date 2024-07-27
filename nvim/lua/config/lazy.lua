5bc935b78c3354ea1935372ea6f51e539344eaff
Kevin Burns <kevin@burnskp.com>
2024-07-26 21:59:39 -0500

    Switch from telescope to fzf

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -22,7 +22,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.coding.yanky" },
 		{ import = "lazyvim.plugins.extras.dap.core" },
 		{ import = "lazyvim.plugins.extras.editor.dial" },
-		{ import = "lazyvim.plugins.extras.editor.telescope" },
+		{ import = "lazyvim.plugins.extras.editor.fzf" },
 		{ import = "lazyvim.plugins.extras.lang.ansible" },
 		{ import = "lazyvim.plugins.extras.lang.git" },
 		{ import = "lazyvim.plugins.extras.lang.go" },
