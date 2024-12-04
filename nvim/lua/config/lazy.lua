bf3a3b0dfc223ca7ade4b6118d71b2e1bc3c8bdc
Kevin Burns <kevin@burnskp.com>
2024-12-04 09:30:35 -0600

    Add ability to disable copilot via env

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -12,36 +12,41 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+local plugins = {
+	{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
+	{ import = "lazyvim.plugins.extras.coding.mini-comment" },
+	{ import = "lazyvim.plugins.extras.coding.mini-surround" },
+	{ import = "lazyvim.plugins.extras.coding.yanky" },
+	{ import = "lazyvim.plugins.extras.dap.core" },
+	{ import = "lazyvim.plugins.extras.editor.dial" },
+	{ import = "lazyvim.plugins.extras.editor.overseer" },
+	{ import = "lazyvim.plugins.extras.lang.ansible" },
+	{ import = "lazyvim.plugins.extras.lang.git" },
+	{ import = "lazyvim.plugins.extras.lang.go" },
+	{ import = "lazyvim.plugins.extras.lang.helm" },
+	{ import = "lazyvim.plugins.extras.lang.json" },
+	{ import = "lazyvim.plugins.extras.lang.markdown" },
+	{ import = "lazyvim.plugins.extras.lang.python" },
+	{ import = "lazyvim.plugins.extras.lang.ruby" },
+	{ import = "lazyvim.plugins.extras.lang.terraform" },
+	{ import = "lazyvim.plugins.extras.lang.yaml" },
+	{ import = "lazyvim.plugins.extras.test.core" },
+	{ import = "lazyvim.plugins.extras.ui.edgy" },
+	{ import = "lazyvim.plugins.extras.ui.treesitter-context" },
+	{ import = "lazyvim.plugins.extras.util.dot" },
+	{ import = "lazyvim.plugins.extras.util.octo" },
+	{ import = "lazyvim.plugins.extras.formatting.prettier" },
+	{ import = "lazyvim.plugins.extras.editor.aerial" },
+	{ import = "plugins" },
+}
+
+if not vim.env.DISABLE_GITHUB_COPILOT then
+	table.insert(plugins, { import = "lazyvim.plugins.extras.ai.copilot" })
+	table.insert(plugins, { import = "lazyvim.plugins.extras.ai.copilot-chat" })
+end
+
 require("lazy").setup({
-	spec = {
-		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
-		{ import = "lazyvim.plugins.extras.ai.copilot" },
-		{ import = "lazyvim.plugins.extras.ai.copilot-chat" },
-		{ import = "lazyvim.plugins.extras.coding.mini-comment" },
-		{ import = "lazyvim.plugins.extras.coding.mini-surround" },
-		{ import = "lazyvim.plugins.extras.coding.yanky" },
-		{ import = "lazyvim.plugins.extras.dap.core" },
-		{ import = "lazyvim.plugins.extras.editor.dial" },
-		{ import = "lazyvim.plugins.extras.editor.overseer" },
-		{ import = "lazyvim.plugins.extras.lang.ansible" },
-		{ import = "lazyvim.plugins.extras.lang.git" },
-		{ import = "lazyvim.plugins.extras.lang.go" },
-		{ import = "lazyvim.plugins.extras.lang.helm" },
-		{ import = "lazyvim.plugins.extras.lang.json" },
-		{ import = "lazyvim.plugins.extras.lang.markdown" },
-		{ import = "lazyvim.plugins.extras.lang.python" },
-		{ import = "lazyvim.plugins.extras.lang.ruby" },
-		{ import = "lazyvim.plugins.extras.lang.terraform" },
-		{ import = "lazyvim.plugins.extras.lang.yaml" },
-		{ import = "lazyvim.plugins.extras.test.core" },
-		{ import = "lazyvim.plugins.extras.ui.edgy" },
-		{ import = "lazyvim.plugins.extras.ui.treesitter-context" },
-		{ import = "lazyvim.plugins.extras.util.dot" },
-		{ import = "lazyvim.plugins.extras.util.octo" },
-		{ import = "lazyvim.plugins.extras.formatting.prettier" },
-		{ import = "lazyvim.plugins.extras.editor.aerial" },
-		{ import = "plugins" },
-	},
+	spec = plugins,
 	defaults = {
 		lazy = false,
 		version = false,
--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -1,69 +1,73 @@
-return {
-	"CopilotC-Nvim/CopilotChat.nvim",
-	opts = {
-		mappings = {
-			reset = {
-				normal = "<C-r>",
-				insert = "<C-r>",
+if vim.env.DISABLE_GITHUB_COPILOT then
+	return {}
+else
+	return {
+		"CopilotC-Nvim/CopilotChat.nvim",
+		opts = {
+			mappings = {
+				reset = {
+					normal = "<C-r>",
+					insert = "<C-r>",
+				},
 			},
 		},
-	},
-	keys = {
-		{
-			"<leader>aa",
-			function()
-				return require("CopilotChat").open({
-					selection = function()
-						return nil
-					end,
-				})
-			end,
-			desc = "Toggle (CopilotChat)",
-			mode = { "n" },
+		keys = {
+			{
+				"<leader>aa",
+				function()
+					return require("CopilotChat").open({
+						selection = function()
+							return nil
+						end,
+					})
+				end,
+				desc = "Toggle (CopilotChat)",
+				mode = { "n" },
+			},
+			{
+				"<leader>aa",
+				function()
+					return require("CopilotChat").open({
+						selection = function(source)
+							return require("CopilotChat.select").visual(source) or nil
+						end,
+					})
+				end,
+				desc = "Toggle (CopilotChat)",
+				mode = { "v" },
+			},
+			{
+				"<leader>ad",
+				"<cmd>CopilotChatDocs<cr>",
+				desc = "Document Code",
+				mode = { "n", "v" },
+			},
+			{
+				"<leader>af",
+				"<cmd>CopilotChatFix<cr>",
+				desc = "Fix Code",
+				mode = { "n", "v" },
+			},
+			{
+				"<leader>ao",
+				"<cmd>CopilotChatOptimize<cr>",
+				desc = "Optimize Code",
+				mode = { "n", "v" },
+			},
+			{
+				"<leader>at",
+				"<cmd>CopilotChatTests<cr>",
+				desc = "Add Tests to Code",
+				mode = { "n", "v" },
+			},
+			{
+				"<leader>as",
+				function()
+					return require("CopilotChat").toggle()
+				end,
+				desc = "Toggle (CopilotChat)",
+				mode = { "n", "v" },
+			},
 		},
-		{
-			"<leader>aa",
-			function()
-				return require("CopilotChat").open({
-					selection = function(source)
-						return require("CopilotChat.select").visual(source) or nil
-					end,
-				})
-			end,
-			desc = "Toggle (CopilotChat)",
-			mode = { "v" },
-		},
-		{
-			"<leader>ad",
-			"<cmd>CopilotChatDocs<cr>",
-			desc = "Document Code",
-			mode = { "n", "v" },
-		},
-		{
-			"<leader>af",
-			"<cmd>CopilotChatFix<cr>",
-			desc = "Fix Code",
-			mode = { "n", "v" },
-		},
-		{
-			"<leader>ao",
-			"<cmd>CopilotChatOptimize<cr>",
-			desc = "Optimize Code",
-			mode = { "n", "v" },
-		},
-		{
-			"<leader>at",
-			"<cmd>CopilotChatTests<cr>",
-			desc = "Add Tests to Code",
-			mode = { "n", "v" },
-		},
-		{
-			"<leader>as",
-			function()
-				return require("CopilotChat").toggle()
-			end,
-			desc = "Toggle (CopilotChat)",
-			mode = { "n", "v" },
-		},
-	},
-}
+	}
+end
