584e2f36e25955ccf70c899829b5e591bdc9f8b7
Kevin Burns <kevin@burnskp.com>
2024-12-04 11:17:18 -0600

    Disable friendly-snippets

--- nvim/lua/plugins/disable.lua
+++ nvim/lua/plugins/disable.lua
@@ -1,6 +1,6 @@
 return {
 	{ "folke/persistence.nvim", enabled = false },
 	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
-	{ "nvimdev/dashboard-nvim", enabled = false },
 	{ "todo-comments.nvim", enabled = false },
+	{ "rafamadriz/friendly-snippets", enabled = false },
 }
