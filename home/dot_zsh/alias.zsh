309265eab8afd910fe48ae1c6185331a8babcadb
Kevin Burns <kevin@burnskp.com>
2025-08-02 11:17:43 -0500

    Remove td talosctl alias

--- home/dot_zsh/alias.zsh
+++ home/dot_zsh/alias.zsh
@@ -51,7 +51,6 @@ fi
 if  [ $commands[talosctl] ]; then
   alias tcg="talosctl config contexts"
   alias tcu="talosctl config context"
-  alias td="talosctl daskboard"
   alias tmem="talosctl memory"
 fi
 
