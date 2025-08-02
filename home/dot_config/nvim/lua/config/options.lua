351cdadb23baa0fb8aed4e9c665b01cde3b4e017
Kevin Burns <kevin@burnskp.com>
2025-08-02 09:06:44 -0500

    Enable trimming of whitespace

--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -69,3 +69,8 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.markdown_recommended_style = 0
+
+-- default editorconfig
+vim.g.editorconfig = {}
+vim.g.editorconfig.trim_trailing_whitespace = true
+vim.g.editorconfig.tab_width = 2
