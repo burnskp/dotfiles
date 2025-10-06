eb74325b583018dc32a6f117a107be4c001d959c
Kevin Burns <kevin@burnskp.com>
2025-10-06 12:28:05 -0500

    Remove catppuccin colors from fzf

--- home/dot_zsh/fzf.zsh
+++ home/dot_zsh/fzf.zsh
@@ -2,13 +2,9 @@
 if [[ $commands[fzf] ]]; then
   source <(fzf --zsh)
   export FZF_DEFAULT_OPTS="--style full --ansi --no-scrollbar \
---color=bg+:#CCD0DA,bg:#EFF1F5,spinner:#DC8A78,hl:#D20F39 \
---color=fg:#4C4F69,header:#D20F39,info:#8839EF,pointer:#DC8A78 \
---color=marker:#7287FD,fg+:#4C4F69,prompt:#8839EF,hl+:#D20F39 \
---color=selected-bg:#BCC0CC --color=border:#CCD0DA,label:#4C4F69 \
 --color=gutter:-1"
 
-  alias fp="fzf --ansi --style full --preview 'bat --color=always --style=plain {}' --no-scrollbar"
+  alias fp="fzf --preview 'bat --color=always --style=plain {}'"
   alias zp=alias zp='cd ~/projects/$(fd . -t d --maxdepth 1 ~/projects --exec basename | fzf)'
 
   function gpr() {
