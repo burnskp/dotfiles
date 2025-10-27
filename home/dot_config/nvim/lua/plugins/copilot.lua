6271c2e926eb314b83f39d32c91f297a2ffa679b
Kevin Burns <kevin@burnskp.com>
2025-10-27 08:46:13 -0500

    Update copilot setup

---
+++ home/dot_config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,12 @@
+return {
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	opts = {
+		sugestion = { enabled = false },
+		panel = { enabled = false },
+		filetypes = {
+			yaml = true,
+		},
+	},
+}
