fdd02ba86320c0b70d5a5a412ca077f62726c2bf
Kevin Burns <kevin@burnskp.com>
2025-10-26 21:17:46 -0500

    Update Linux configs

--- home/dot_zsh/cachyos.zsh
+++ home/dot_zsh/cachyos.zsh
@@ -1,14 +1,15 @@
 #!/usr/zsh
 if [ "$(uname -o)" == "GNU/Linux" ] && grep -q Arch /etc/*release 2>&1 >/dev/null; then
   alias pac="sudo pacman -S"
+  alias paci="pacman -Si"
   alias pacq="pacman -Q"
   alias pacr="sudo pacman -Rs"
+  alias pacrz="pacman -Qq | fzf --multi --preview 'pacman -Qi {1}' | xargs -ro sudo pacman -Rns"
   alias pacs="pacman -Ss"
   alias pacsy="sudo pacman -Sy"
   alias pacsyu="sudo pacman -Syu"
   alias pacu="sudo pacman -U"
   alias pacz="pacman -Slq | fzf --multi --preview 'pacman -Si {1}' | xargs -ro sudo pacman -S"
-  alias pacrz="pacman -Qq | fzf --multi --preview 'pacman -Qi {1}' | xargs -ro sudo pacman -Rns"
   alias paru="paru -S"
   alias parus="paru -Ss"
   alias paruu="paru -Su"
