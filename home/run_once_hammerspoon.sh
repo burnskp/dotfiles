f4b2d6ddbcce76a95432243c6142edabe2227cc6
Kevin Burns <kevin@burnskp.com>
2025-11-05 12:47:14 -0600

    Fix hammerspoon init path

--- home/run_once_hammerspoon.sh
+++ home/run_once_hammerspoon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [[ "$(uname -o)" == "Darwin" ]]; then
-  defaults write org.hammerspoon.Hammerspoon MJConfigFile "$XDG_CONFIG_HOME/hammerspoon/init"
+  defaults write org.hammerspoon.Hammerspoon MJConfigFile "$XDG_CONFIG_HOME/hammerspoon/init.lua"
 fi
