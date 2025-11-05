06e58e243432dd749f83ef7367d6f874f6d5b3cc
Kevin Burns <kevin@burnskp.com>
2025-11-04 19:58:42 -0600

    Update conform config

--- home/dot_config/nvim/lua/plugins/conform.lua
+++ home/dot_config/nvim/lua/plugins/conform.lua
@@ -1,14 +1,24 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      ["json"] = { "fixjson" },
-      ["*"] = { "trim_whitespace" },
-    },
-    formatters = {
-      shfmt = {
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "cbfmt",
+        "fixjson",
+        "mdformat",
+      })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft["json"] = { "fixjson" }
+      opts.formatters_by_ft["*"] = { "trim_whitespace" }
+      opts.formatters_by_ft["markdown"] = { "mdformat", "markdownlint-cli2", "markdown-toc", "cbfmt" }
+      opts.formatters_by_ft["markdown.mdx"] = { "mdformat", "markdownlint-cli2", "markdown-toc", "cbfmt" }
+      opts.formatters.sfmt = {
         prepend_args = { "-ci", "-s", "-i", "2", "-bn", "-sr" },
-      },
-    },
+      }
+    end,
   },
 }
