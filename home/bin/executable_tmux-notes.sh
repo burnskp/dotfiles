276ccb813c5d79fd906005dad359ee5fbe201aad
Kevin Burns <kevin@burnskp.com>
2025-11-06 21:37:40 -0600

    Update notes setup

--- home/bin/executable_tmux-notes.sh
+++ home/bin/executable_tmux-notes.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-NOTES_DIR="$HOME/notes"
+NOTES_DIR="$XDG_DATA_HOME/notes"
 EXT=".md"
 
 mkdir -p "$NOTES_DIR"
 
 # Lua code to set note-friendly options
-read -r -d '' NOTE_LUA << 'EOF'
+read -r -d '' NOTE_LUA <<'EOF'
 vim.opt_local.number = false
 vim.opt_local.relativenumber = false
 vim.opt_local.signcolumn = "no"
@@ -30,8 +30,8 @@ EOF
 popup() {
   session="notes"
 
-  if ! tmux has -t "$session" 2> /dev/null; then
-    session_id="$(tmux new-session -dP -s "$session" -F '#{session_id}' "$HOME/bin/tmux-notes.sh" _popup "$1")"
+  if ! tmux has -t "$session" 2>/dev/null; then
+    session_id="$(tmux new-session -dP -s "$session" -F '#{session_id}' tmux-notes.sh _popup "$1")"
     tmux set-option -s -t "$session_id" key-table popup
     tmux set-option -s -t "$session_id" status off
     tmux set-option -s -t "$session_id" prefix None
@@ -39,7 +39,7 @@ popup() {
     session="$session_id"
   fi
 
-  exec tmux attach -t "$session" > /dev/null
+  exec tmux attach -t "$session" >/dev/null
 }
 
 commit_note() {
@@ -54,7 +54,7 @@ open_note() {
   # Write the Lua config to a temp file
   local lua_tmp
   lua_tmp=$(mktemp /tmp/note_lua.XXXXXX.lua)
-  echo "$NOTE_LUA" > "$lua_tmp"
+  echo "$NOTE_LUA" >"$lua_tmp"
 
   if [[ -n $file && -f $file ]]; then
     nvim --cmd "luafile $lua_tmp" +'nnoremap q :wq<CR>' "$file"
@@ -69,17 +69,19 @@ open_note() {
 }
 
 find_notes() {
-  open_note "$(FZF_DEFAULT_COMMAND="fd -I '$EXT' '$NOTES_DIR'" \
+  cd "$NOTES_DIR" || exit 1
+  open_note "$(FZF_DEFAULT_COMMAND="fd -I '$EXT'" \
     fzf --prompt="Find note: " \
     --preview="bat --style=plain --color=always --line-range=:40 {}" \
     --preview-window=up:40%:wrap \
     --bind "ctrl-n:accept" \
-    --bind "ctrl-g:reload:rg --no-ignore --ignore-case --files-with-matches {q} '$NOTES_DIR/**/*.md' 2>/dev/null || true" \
-    --print-query)"
+    --bind "ctrl-g:reload:rg --no-ignore --ignore-case --files-with-matches {q} '**/*.md' 2>/dev/null || true" \
+    --print-query | sed "s|$NOTES_DIR/||g")"
 }
 
 grep_notes() {
-  RG_PREFIX="rg --no-ignore --ignore-case --files-with-matches {q} '$NOTES_DIR'/**/*.md"
+  RG_PREFIX="rg --no-ignore --ignore-case --files-with-matches {q} **/*.md"
+  cd "$NOTES_DIR" || exit 1
   result=$(fzf --prompt="Grep notes: " --bind "start:reload:$RG_PREFIX" \
     --bind "change:reload:$RG_PREFIX|| true" \
     --preview="bat --style=plain --color=always --line-range=:40 {}" \
--- home/dot_config/nvim/lua/plugins/notes.lua
+++ home/dot_config/nvim/lua/plugins/notes.lua
@@ -2,6 +2,9 @@ return {
   {
     "burnskp/notes.nvim",
     opts = {
+      notesDir = "$XDG_DATA_HOME/notes/global",
+      projectNotesDir = "$XDG_DATA_HOME/notes/projects",
+      journalDir = "$XDG_DATA_HOME/notes/journal",
       git = {
         auto_commit = true,
         auto_push = true,
@@ -9,11 +12,14 @@ return {
     },
     cmd = {
       "CreateNote",
+      "Journal",
+      "JournalFind",
+      "JournalGrep",
+      "LastNote",
       "Notes",
       "NotesAll",
       "NotesAllGrep",
       "NotesGrep",
-      "LastNote",
       "ProjectNote",
       "ProjectNotes",
       "ProjectNotesGrep",
@@ -32,28 +38,10 @@ return {
       { "<leader>nS", "<cmd>ProjectNote scratch<CR>", desc = "Grep Project NotNote - Scratch" },
       { "<leader>nt", "<cmd>ProjectNote todo float<CR>", desc = "Project Note - Todo (Float)" },
       { "<leader>nT", "<cmd>ProjectNote todo<CR>", desc = "Grep Project NotNote - Todo" },
+      { "<leader>nt", "<cmd>Journal float<CR>", desc = "Today's Journal (Float)" },
+      { "<leader>nT", "<cmd>Journal<CR>", desc = "Today's Journal" },
+      { "<leader>nj", "<cmd>JournalGrep float<CR>", desc = "Grep Journal Entries (Float)" },
+      { "<leader>nJ", "<cmd>JournalGrep<CR>", desc = "Grep Journal Entries" },
     },
-  },
-  {
-    "folke/snacks.nvim",
-    opts = function(_, opts)
-      local keys = {
-        { icon = " ", key = "s", desc = "Search Notes", action = ":NotesGrep" },
-        { icon = " ", key = "n", desc = "Notes", action = ":Notes" },
-        { icon = " ", key = "e", desc = "New File", action = ":ene | startinsert" },
-      }
-
-      for _, key in ipairs(keys) do
-        local config_index = 2
-        for i = #opts.dashboard.preset.keys, 1, -1 do
-          if opts.dashboard.preset.keys[i].key == key.key then
-            table.remove(opts.dashboard.preset.keys, i)
-            break
-          end
-        end
-        table.insert(opts.dashboard.preset.keys, config_index, key)
-        config_index = config_index + 1
-      end
-    end,
   },
 }
