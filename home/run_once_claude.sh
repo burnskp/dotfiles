072b3d2d6425f42dc7f4e33e7a8c50b751ba3491
Kevin Burns <kevin@burnskp.com>
2025-11-06 20:16:19 -0600

    Switch claude install to use npm

--- home/run_once_claude.sh
+++ home/run_once_claude.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 if [[ "$(uname -o)" == "GNU/Linux" ]]; then
-  export BUN_INSTALL="$HOME/.local/share/bun"
-  bun add -g "@anthropic-ai/claude-code"
+  npm install -g "@anthropic-ai/claude-code"
 fi
