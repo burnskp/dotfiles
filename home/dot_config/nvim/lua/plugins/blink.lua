9c84733ba17f7ea9d4226ea21a0e0f633704e228
Kevin Burns <kevin@burnskp.com>
2025-11-08 09:54:35 -0600

    Change how cmp works inside snippets

--- home/dot_config/nvim/lua/plugins/blink.lua
+++ home/dot_config/nvim/lua/plugins/blink.lua
@@ -1,6 +1,15 @@
 return {
   "saghen/blink.cmp",
   opts = {
+    completion = {
+      list = {
+        selection = {
+          preselect = function()
+            return not require("blink.cmp").snippet_active({ direction = 1 })
+          end,
+        },
+      },
+    },
     keymap = {
       preset = "super-tab",
       ["<C-k>"] = { "show", "show_documentation", "hide_documentation" },
