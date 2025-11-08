38cfacbd6cac861508662bbe723c8ddbd3163602
Kevin Burns <kevin@burnskp.com>
2025-11-08 10:17:53 -0600

    Test out blink.indent

---
+++ home/dot_config/nvim/lua/plugins/blink-indent.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      indent = { enabled = false },
+    },
+  },
+  {
+    "saghen/blink.indent",
+    opts = {
+      -- static = {
+      --   char = "│",
+      -- },
+      -- scope = {
+      --   char = "│",
+      -- },
+    },
+  },
+}
