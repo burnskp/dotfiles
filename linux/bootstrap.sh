1c2fb9c31c77a06610d11ef895b3494a3c9f6770
Kevin Burns <kevin@burnskp.com>
2025-12-19 09:57:55 -0600

    Add ansible play to manage setup

--- linux/bootstrap.sh
+++ linux/bootstrap.sh
@@ -1,5 +1,14 @@
-sudo pacman -Syu
-cat packages.txt | sudo pacman -S --needed -
+#!/bin/bash
+set -e
+if ! command -v ansible &> /dev/null; then
+
+  echo "Ansible not found. Installing..."
+  sudo pacman -Syu --noconfirm ansible
+  ansible-galaxy collection install community.general
+fi
+
+ansible-playbook playbook.yml --ask-become-pass
+
 if [[ -f "aur.txt" ]]; then
-cat aur.txt | sudo paru -S --needed -
-chezmoi init --apply https://github.com/burnskp/dotfiles.git
+  cat aur.txt | sudo paru -S --needed -
+fi
