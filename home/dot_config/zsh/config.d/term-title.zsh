7f6ad6baed833ad1bfe7496d8282770a20639899
Kevin Burns <kevin@burnskp.com>
2026-02-02 09:58:50 -0600

    only use term-title when running tmux

--- home/dot_config/zsh/config.d/term-title.zsh
+++ home/dot_config/zsh/config.d/term-title.zsh
@@ -2,16 +2,22 @@ typeset -a excluded_commands
 excluded_commands=('ls' 'cd' 'echo' 'pwd' 'git' 'cat' 'bat')
 
 function title-precmd() {
+  [[ -z $TMUX ]] && return
+  local dir
   if [[ $PWD == "$HOME" ]]; then
-    print -n "\ekd:~\e\\"
+    dir="~"
   else
-    print -n "\ekd:${PWD##*/}\e\\"
+    dir="${PWD##*/}"
   fi
+  [[ ${#dir} -gt 30 ]] && dir="${dir:0:27}..."
+  print -n "\ekd:${dir}\e\\"
 }
 
 function title-preexec() {
+  [[ -z $TMUX ]] && return
   cmd_name="${1%% *}"
   if [[ ! ${excluded_commands[(r)$cmd_name]} ]]; then
+    [[ ${#cmd_name} -gt 30 ]] && cmd_name="${cmd_name:0:27}..."
     print -n "\ek${cmd_name}\e\\"
   fi
 }
